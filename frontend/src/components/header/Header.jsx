2453501724384b11b192d1e4199c083789ac3200
Rajeevkumarmodi <125949049+Rajeevkumarmodi@users.noreply.github.com>
2023-10-12 12:06:56 +0530

    header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 function Header() {
   return (
-    <div>
-      <h1>this is hader</h1>
+    <div className=" bg-black w-full px-[40px] md:px-[80px]">
+      <h2 className=" text-white text-2xl py-2 font-bold">
+        User
+        <span className="text-blue-400 ml-3">CRUD</span>
+      </h2>
     </div>
   );
 }
